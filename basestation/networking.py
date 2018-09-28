74e23e190e70f263eee041eea4a3f70a4cb840a8
Reece Berens <rberens@ksu.edu>
2018-09-27 19:51:39 -0500

    Begin network class in basestation

--- basestation/networking.py
+++ basestation/networking.py
@@ -1,3 +1,11 @@
 """
 	File that will hold the networking class to interface with the web server.
 """
+
+class Network:
+		def connectToWeb(self):
+			print "TODO: Implement connectToWeb function in basestation/networking.py"
+
+		def sendMessage(self, messageToSend):
+			print "TODO: Implement sendMessage function in basestation/networking.py"
+		
