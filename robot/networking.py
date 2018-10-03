b8a298fcf21a5de226a61be94a4f28c83cc1f50b
Reece Berens <rberens@ksu.edu>
2018-10-03 17:21:16 -0500

    Added networking methods and main loop to robot

--- robot/networking.py
+++ robot/networking.py
@@ -2,3 +2,26 @@
     Class that will hold all networking information for the Pi.
     This class will define the GET request for controller data, as well as the UPDATE request for sensor data.
 """
+import asyncio
+import requests
+
+"""
+    This method will be used to GET controller data from the web server
+    Once the data is received from the server, it is returned
+    Ideally, this will run asynchronously
+    loop argument should come from the main loop to keep consistency (I guess)
+"""
+async def getControllerStatus(loop):
+    url = "OurWebServerAddress"
+    r = requests.get(url)
+    print(r.text)
+    return r.text #this is the data that is returned from the web server
+
+"""
+    This method will be used to PUT sensor data onto the web server
+    Ideally, this method will be run asynchronously
+    loop argument should come from the main loop to keep consistency (I guess)
+"""
+async def updateSensorData(loop, sensorData):
+    url = "OurWebServerAddress"
+    r = requests.put(url, data = sensorData)
--- robot/robot.py
+++ robot/robot.py
@@ -4,3 +4,21 @@
 	The robot will also publish sensor data to the web service for the basestation. 
 	We will maybe have two separate Pis for camera and motors/servos etc. The camera Pi should have its own folder and code and such
 """
+import asyncio
+import logging
+import networking
+
+def mainLoop():
+	logging.basicConfig(format="%(name)s: %(levelname)s: %(asctime)s: %(message)s", level=logging.INFO)
+	logger = logging.getLogger(__name__)
+	loop = asyncio.get_event_loop()
+
+	try:
+		loop.run_forever()
+	except KeyboardInterrupt:
+		logger.info("Keyboard interrupt detected. Exiting now.")
+	finally:
+		#ensure robot loop gets closed
+
+		loop.close()
+		logger.info("Event loop closed. Exiting program")
