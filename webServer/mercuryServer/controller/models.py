6dc1262416482a2386343be2c61c2aafe5879ab8
Reece Berens <rberens@ksu.edu>
2019-01-24 20:00:22 -0600

    Shortened controller fields on web server

--- webServer/mercuryServer/controller/models.py
+++ webServer/mercuryServer/controller/models.py
@@ -5,34 +5,34 @@ class ControllerInput(models.Model):
     b = models.IntegerField(default = 0)
     x = models.IntegerField(default = 0)
     y = models.IntegerField(default = 0)
-    start = models.IntegerField(default = 0)
-    select = models.IntegerField(default = 0)
-    r_trigger = models.IntegerField(default = 0)
-    l_trigger = models.IntegerField(default = 0)
-    r_bump = models.IntegerField(default = 0)
-    l_bump = models.IntegerField(default = 0)
-    r_stick_x = models.IntegerField(default = 0)
-    r_stick_y = models.IntegerField(default = 0)
-    l_stick_x = models.IntegerField(default = 0)
-    l_stick_y = models.IntegerField(default = 0)
-    up = models.IntegerField(default = 0)
-    down = models.IntegerField(default = 0)
-    left = models.IntegerField(default = 0)
-    right = models.IntegerField(default = 0)
+    st = models.IntegerField(default = 0)
+    se = models.IntegerField(default = 0)
+    rt = models.IntegerField(default = 0)
+    lt = models.IntegerField(default = 0)
+    rb = models.IntegerField(default = 0)
+    lb = models.IntegerField(default = 0)
+    rsx = models.IntegerField(default = 0)
+    rsy = models.IntegerField(default = 0)
+    lsx = models.IntegerField(default = 0)
+    lsy = models.IntegerField(default = 0)
+    u = models.IntegerField(default = 0)
+    d = models.IntegerField(default = 0)
+    l = models.IntegerField(default = 0)
+    r = models.IntegerField(default = 0)
     
 
     def __str__(self):
-        val = "a: {} | b: {} | x: {} | y: {} | start: {} | select: {}\n".format(self.a, self.b, self.x, self.y, self.start, self.select)
-        val += "r_trigger: {} | l_trigger: {} | r_bump: {} | l_bump: {}\n".format(self.r_trigger, self.l_trigger, self.r_bump, self.l_bump)
-        val += "r_stick_x: {} | r_stick_y: {} | l_stick_x: {} | l_stick_y: {}".format(self.r_stick_x, self.r_stick_y, self.l_stick_x, self.l_stick_y)
-        val += "upDPad: {} | downDPad: {} | leftDPad: {} | rightDPad: {}".format(self.up, self.down, self.left, self.right)
+        val = "a: {} | b: {} | x: {} | y: {} | st: {} | se: {}\n".format(self.a, self.b, self.x, self.y, self.st, self.se)
+        val += "rt: {} | lt: {} | rb: {} | lb: {}\n".format(self.rt, self.lt, self.rb, self.lb)
+        val += "rsx: {} | rsy: {} | lsx: {} | lsy: {}".format(self.rsx, self.rsy, self.lsx, self.lsy)
+        val += "u: {} | d: {} | l: {} | r: {}".format(self.u, self.d, self.l, self.r)
         return val
 
     def createDictionary(self):
         value = {
-        'a':self.a, 'b': self.b, 'x': self.x, 'y': self.y, 'start': self.start, 'select': self.select,
-        'r_trigger': self.r_trigger, 'l_trigger': self.l_trigger, 'r_bump': self.r_bump, 'l_bump': self.l_bump,
-        'r_stick_x': self.r_stick_x, 'r_stick_y': self.r_stick_y, 'l_stick_x': self.l_stick_x, 'l_stick_y': self.l_stick_y,
-        'up': self.up, 'down': self.down, 'left': self.left, 'right': self.right,
+        'a':self.a, 'b': self.b, 'x': self.x, 'y': self.y, 'start': self.st, 'select': self.se,
+        'r_trigger': self.rt, 'l_trigger': self.lt, 'r_bump': self.rb, 'l_bump': self.lb,
+        'r_stick_x': self.rsx, 'r_stick_y': self.rsy, 'l_stick_x': self.lsx, 'l_stick_y': self.lsy,
+        'up': self.u, 'down': self.d, 'left': self.l, 'right': self.r,
         }
         return value
