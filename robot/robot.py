fdb0cd04281f496c4ae25f9df4c66a343e1eb005
Dustin Wilson <rberens@ksu.edu>
2019-02-18 19:26:32 -0600

    Motors move as long as you use the correct pi unlike Reece

--- robot/robot.py
+++ robot/robot.py
@@ -11,6 +11,7 @@ from threading import Thread
 from networking import RobotNetwork
 from components import *
 from settings import * # this gets us constants such as WEB_SERVER_ADDRESS
+import RPi.GPIO as GPIO
 
 class Robot:
     def __init__(self):
@@ -75,9 +76,11 @@ class Robot:
         finally:
             #ensure robot loop gets closed
             self.logger.info("Event loop closed. Exiting program")
+            GPIO.cleanup()
 
     def updateOutputComponents(self):
         for c in self.outputComponentList:
+            print(self.controllerData[c.controllerInput])
             c.doUpdate(self.controllerData[c.controllerInput], self.controllerData[c.backwardInput])
 
     def updateSensorValues(self):
@@ -109,8 +112,12 @@ class Robot:
             self.updateOutputComponentsTEST()
 
 if __name__ == '__main__':
-    r = Robot()
-    r.mainLoop()
+    try:
+        r = Robot()
+        r.mainLoop()
+    except:
+        print("ERROR")
+        GPIO.cleanup()
 
     """t1 = Thread(target=r.outputComponentThreadMethod, name='OUTPUT-COMP-THREAD')
     t2 = Thread(target=r.sensorUpdateThreadMethod, name='SENSOR-THREAD')
