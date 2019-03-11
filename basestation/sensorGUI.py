3c4580b5ff251b03498e75dd4eb505dfc2758cce
Reece Berens <rberens@ksu.edu>
2019-03-11 16:59:26 -0500

    Updated sensor GUI with new layout, need to update robot and web server with new dictionary

--- basestation/sensorGUI.py
+++ basestation/sensorGUI.py
@@ -20,62 +20,69 @@ class SensorGUI(tk.Frame):
         self.tv_qfl = tk.StringVar()
         self.tv_qfl.set(self.t_qfl + str(-1))
         self.lbl_qfl = tk.Label(self, textvariable=self.tv_qfl)
-        self.lbl_qfl.grid(column=0, row=0)
+        self.lbl_qfl.grid(column=0, row=1, padx=5, pady=5)
 
         #Front right quad encoder
         self.t_qfr = "Quad Front Right: "
         self.tv_qfr = tk.StringVar()
         self.tv_qfr.set(self.t_qfr + str(-1))
         self.lbl_qfr = tk.Label(self, textvariable=self.tv_qfr)
-        self.lbl_qfr.grid(column=2, row=0)
+        self.lbl_qfr.grid(column=4, row=1, padx=5, pady=5)
 
         #Back left quad encoder
         self.t_qbl = "Quad Back Left: "
         self.tv_qbl = tk.StringVar()
         self.tv_qbl.set(self.t_qbl + str(-1))
         self.lbl_qbl = tk.Label(self, textvariable=self.tv_qbl)
-        self.lbl_qbl.grid(column=0, row=3)
+        self.lbl_qbl.grid(column=0, row=5, padx=5, pady=5)
 
         #Back right quad encoder
         self.t_qbr = "Quad Back Right: "
         self.tv_qbr = tk.StringVar()
         self.tv_qbr.set(self.t_qbr + str(-1))
         self.lbl_qbr = tk.Label(self, textvariable=self.tv_qbr)
-        self.lbl_qbr.grid(column=2, row=3)
+        self.lbl_qbr.grid(column=4, row=5, padx=5, pady=5)
 
-        #Front distance sensor
-        self.t_df = "Distance Front: "
-        self.tv_df = tk.StringVar()
-        self.tv_df.set(self.t_df + str(-1))
-        self.lbl_df = tk.Label(self, textvariable=self.tv_df)
-        self.lbl_df.grid(column=1, row=0)
+        #Front left distance sensor
+        self.t_dfl = "Front left: "
+        self.tv_dfl = tk.StringVar()
+        self.tv_dfl.set(self.t_dfl + str(-1))
+        self.lbl_dfl = tk.Label(self, textvariable=self.tv_dfl)
+        self.lbl_dfl.grid(column=1, row=1, padx=5, pady=5)
 
-        #Back distance sensor
-        self.t_db = "Distance Back: "
-        self.tv_db = tk.StringVar()
-        self.tv_db.set(self.t_db + str(-1))
-        self.lbl_db = tk.Label(self, textvariable=self.tv_db)
-        self.lbl_db.grid(column=1, row=3)
+        #Left side distance sensor
+        self.t_dsl = "Left Side: "
+        self.tv_dsl = tk.StringVar()
+        self.tv_dsl.set(self.t_dsl + str(-1))
+        self.lbl_dsl = tk.Label(self, textvariable=self.tv_dsl)
+        self.lbl_dsl.grid(column=0, row=2, padx=5, pady=5)
 
-        #Left distance sensor
-        self.t_dl = "Distance Left: "
-        self.tv_dl = tk.StringVar()
-        self.tv_dl.set(self.t_dl + str(-1))
-        self.lbl_dl = tk.Label(self, textvariable=self.tv_dl)
-        self.lbl_dl.grid(column=0, row=1)
+        #Front right distance sensor
+        self.t_dfr = "Front Right: "
+        self.tv_dfr = tk.StringVar()
+        self.tv_dfr.set(self.t_dfr + str(-1))
+        self.lbl_dfr = tk.Label(self, textvariable=self.tv_dfr)
+        self.lbl_dfr.grid(column=3, row=1, padx=5, pady=5)
 
-        #Right distance sensor
-        self.t_dr = "Distance Right: "
-        self.tv_dr = tk.StringVar()
-        self.tv_dr.set(self.t_dr + str(-1))
-        self.lbl_dr = tk.Label(self, textvariable=self.tv_dr)
-        self.lbl_dr.grid(column=2, row=1)
+        #Right side distance sensor
+        self.t_dsr = "Right Side: "
+        self.tv_dsr = tk.StringVar()
+        self.tv_dsr.set(self.t_dsr + str(-1))
+        self.lbl_dsr = tk.Label(self, textvariable=self.tv_dsr)
+        self.lbl_dsr.grid(column=4, row=2, padx=5, pady=5)
+
+        self.t_da = "Arm: "
+        self.tv_da = tk.StringVar()
+        self.tv_da.set(self.t_da + str(-1))
+        self.lbl_da = tk.Label(self, textvariable=self.tv_da)
+        self.lbl_da.grid(column=0, row=0, padx=5, pady=5)
     
     def updateSensorValues(self, valueDict):
-        self.tv_db.set(self.t_db + str(valueDict["db"]))
-        self.tv_df.set(self.t_df + str(valueDict['df']))
-        self.tv_dl.set(self.t_dl + str(valueDict['dl']))
-        self.tv_dr.set(self.t_dr + str(valueDict['dr']))
+        self.tv_dfl.set(self.t_dfl + str(valueDict['dfl']))
+        self.tv_dfr.set(self.t_dfr + str(valueDict['dfr']))
+        self.tv_dsl.set(self.t_dfl + str(valueDict['dsl']))
+        self.tv_dsr.set(self.t_dsl + str(valueDict['dsr']))
+        self.tv_da.set(self.t_da + str(valueDict['da']))
         self.tv_qbl.set(self.t_qbl + str(valueDict['qbl']))
         self.tv_qbr.set(self.t_qbr + str(valueDict['qbr']))
         self.tv_qfl.set(self.t_qfl + str(valueDict['qfl']))
@@ -83,10 +90,11 @@ class SensorGUI(tk.Frame):
 
     def testUpdate(self):
         valueDict = {
-            'db': random.randint(0, 100),
-            'df': random.randint(0, 100),
-            'dl': random.randint(0, 100),
-            'dr': random.randint(0, 100),
+            'dfl': random.randint(0, 100),
+            'dfr': random.randint(0, 100),
+            'dsl': random.randint(0, 100),
+            'dsr': random.randint(0, 100),
+            'da': random.randint(0, 100),
             'qbl': random.randint(0, 100),
             'qbr': random.randint(0, 100),
             'qfl': random.randint(0, 100),
@@ -103,6 +111,6 @@ class SensorGUI(tk.Frame):
 
 if __name__ == '__main__':
     gui = SensorGUI()
-    gui.master.title('Sensors!!')
+    gui.master.title('Sensors')
     gui.after(1000, gui.testUpdate) #Used to make sure everything updates
     gui.mainloop()
