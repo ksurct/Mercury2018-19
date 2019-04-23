03ba62418dddbdc2421b9496ff35c344d858044a
Reece Berens <rberens@ksu.edu>
2019-04-23 15:53:07 -0500

    Added new reconnect to robot

---
+++ robot/reconnect.py
@@ -0,0 +1,17 @@
+import os, requests
+from time import sleep
+
+
+# This file automatically reconnects the wifi on the pi if it drops, used for LOS tests
+# DO NOT RUN THIS FILE WITHOUT A & AT THE END OF THE FILE BECAUSE THERE IS AN INFINITE LOOP SO THE REST OF THE SYSTEM WON'T START I ALREADY MADE THIS MISTAKE PLEASE DON'T DO IT AGAIN
+
+while True:
+    try:
+        r = requests.get('http://google.com')
+        if (r.status_code == 200):
+            print("Good response")
+        else:
+            os.system('sudo sh reconnect.sh')
+        sleep(5)
+    except (ConnectionRefusedError, ConnectionResetError, requests.exceptions.ConnectionError):
+        os.system('sudo sh reconnect.sh')
--- robot/reconnect.sh
+++ robot/reconnect.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-# This file automatically reconnects the wifi on the pi if it drops, used for LOS tests
-# DO NOT RUN THIS FILE WITHOUT A & AT THE END OF THE FILE BECAUSE THERE IS AN INFINITE LOOP SO THE REST OF THE SYSTEM WON'T START I ALREADY MADE THIS MISTAKE PLEASE DON'T DO IT AGAIN
 
-while true ; do
-	if ifconfig wlan0 | grep -q "inet" ; then
-		#echo "Network is fine"
-		sleep 5
-	else
-		#echo "Network needs to restart"
-		sudo ifconfig wlan0 up
-		sleep 10
-	fi
-done
+
+#while true ; do
+#	if ifconfig wlan0 | grep -q "inet" ; then
+#		#echo "Network is fine"
+#		sleep 5
+#	else
+#		#echo "Network needs to restart"
+#		sudo ifconfig wlan0 up
+#		sleep 10
+#	fi
+#done
+
+sudo ifconfig wlan0 down
+sleep 10
+sudo ifconfig wlan0 up
+sleep 5
